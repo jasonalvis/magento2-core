22b9a57daa22441520999dd1ffb33ce9313bfea6
Jason Alvis <alvis.jason@gmail.com>
2017-11-29 00:06:59 +0000

    package name change to camel case JasonAlvis

--- registration.php
+++ registration.php
@@ -2,14 +2,14 @@
 /**
  * Jason Alvis Core
  *
- * @category    Jasonalvis
- * @package     Jasonalvis_Core
+ * @category    JasonAlvis
+ * @package     JasonAlvis_Core
  * @author      Jason Alvis
  * @copyright   Copyright (c) 2016 Jason Alvis (http://jasonalvis.co.uk)
  * @license     http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Jasonalvis_Core',
+    'JasonAlvis_Core',
     __DIR__
 );
